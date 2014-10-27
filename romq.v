82d1cccc4def92de08fde5ea695d2f932d5f44b3
guptar52 <guptar52@students.rowan.edu>
2014-10-27 15:05:34 -0400

    Improved comments
    Added better comments to romq and romq_test.

--- romq.v
+++ romq.v
@@ -4,12 +4,13 @@ This ROM stores the inverse of quantization values (8 bits, unsigned).
 Although organized as 8 × 64 bits, it is byte-addressed (64 × 8 bits) while 
 reading. 
 */ 
+
 module romq (clk, a, d); 
  input clk; // Declare I/Os. 
- input [5:0] a; // This is the 6-bit address and 
- output [7:0] d; // data output of ROM. 
- reg [7:0] d; // Declare as the register. 
- wire [7:0] d_next ; // Declare as the wire. 
+ input [5:0] a; //a is the 6 bit input address bus 
+ output [7:0] d; //d is the 8 bit output data bus
+ reg [7:0] d; // Declare as the register so it can be changed quickly. 
+ wire [7:0] d_next ; // Declare as the wire to send data out 
  
  reg [63:0] mem [7:0] ; // ROM organized as 8x64 
  reg [7:0] byte_data [7:0] ; // bits, but read byte-by-byte.
@@ -23,7 +24,7 @@ module romq (clk, a, d);
  wire [63:0] loc5 ; 
  wire [63:0] loc6 ; 
  wire [63:0] loc7 ; 
- 
+ //Actual data for the memory to save
  assign loc0 = 64'hFF806C5D4F4C473C ; 
  assign loc1 = 64'h80805D554C473C37 ; 
  assign loc2 = 64'h6C5D4F4C473C3C36 ; 
@@ -32,11 +33,12 @@ module romq (clk, a, d);
  assign loc5 = 64'h4F4C47403B332B23 ; 
  assign loc6 = 64'h4F4C473C362D251E ; 
  assign loc7 = 64'h4C473B362D251E19 ; 
- 
+//End initialization 
  
 always @ (loc0 or loc1 or loc2 or loc3 or loc4 or loc5 or loc6 or loc7) 
 begin 
  // Bytes from each row is accessed in a raster scan order (MSB first, etc). 
+ // Raster scan works from bottom right to top left.
  mem [0] = loc0 ; 
  mem [1] = loc1 ; 
  mem [2] = loc2 ; 
@@ -46,18 +48,22 @@ begin
  mem [6] = loc6 ; 
  mem [7] = loc7 ; 
 end 
- 
+
+//If mem_data has data to transfer, run this block
+//Choose byte_data based on the LSB 3 bit bus of 'a'. 
 always @ (mem_data) 
 begin 
-byte_data [0] = mem_data [63:56] ; // MSB is assigned as 
-byte_data [1] = mem_data [55:48] ; // LSB. 
+byte_data [0] = mem_data [63:56] ; // Swap MSB and LSB because of raster scan format
+byte_data [1] = mem_data [55:48] ; 
 byte_data [2] = mem_data [47:40] ; 
 byte_data [3] = mem_data [39:32] ; 
 byte_data [4] = mem_data [31:24] ; 
 byte_data [5] = mem_data [23:16] ; 
 byte_data [6] = mem_data [15:8] ; 
-byte_data [7] = mem_data [7:0] ; // LSB is assigned as MSB. 
+byte_data [7] = mem_data [7:0] ;  
 end 
+
+//To be performed when an address is called
 assign mem_data = mem [a[5:3]] ; // Get 64 bits data. 
 assign d_next = byte_data [a[2:0]] ; // Get byte data. 
 always @ (posedge clk) 
--- romq_test.v
+++ romq_test.v
@@ -1,9 +1,11 @@
 //Will call every address of the romq and receive the inverse quantization matrix 
-
+//To test, read the output at d and compare with solution
 `define clkperiodby2 5 // Specify the half time as 5ns, making the clock 100MHz 
-`include ?romq_banno.v? // Use the back annotated version of 
-// romq.v for testing the design. 
+`include "romq.v" //Include the file to be tested; in this case, romq
+
 module romq_test (d); // The test module is declared. 
+
+
 output [7:0] d ; // So also the output. 
 reg clk ; // The inputs are declared as 
 reg [5:0] a ; // ?reg? in a test bench. 
@@ -13,16 +15,18 @@ integer count ; // count is an integer variable.
 
 //Address calling loop
 initial begin 
-clk = 1'b0 ; // Initialize the clock and 
-#7 a = 0 ; // address.  
+clk = 1'b0 ; // Initialize the clock 
+#7 a = 0 ;  //Initialize address with a slight delay
+
 for (count = 0; count < 64; count = count+1) // count = 0?63. 
 #10 a = count ; // Apply new address 
- // every 10 ns a = 0?63. 
+ // every 10 ns a++. 
 #200 // Stop after some time. 
 $stop ; 
 end 
 
+
 //Clock always block
 always 
- #`clkperiodby2 clk <= ~clk ; // Generate 100 MHz clock. 
+ #`clkperiodby2 clk <= ~clk ; // Generate the 100 MHz master clock. 
 endmodule 
