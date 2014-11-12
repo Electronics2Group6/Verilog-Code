5e4f6f4b9773bfc9e507cbd93ff5e64350111657
guptar52 <guptar52@students.rowan.edu>
2014-11-12 13:15:55 -0500

    updated multipliers

--- Adder stuff/mult11sx8s_test.v
+++ Adder stuff/mult11sx8s_test.v
@@ -1,45 +1,43 @@
 `define clkperiodby2 10 // Simulate at 50 MHz. 
 `include "mult11sx8s.v" // Back annotated design file. 
 module mult11sx8s_test (result); 
+
   output [18:0] result ;  
   reg clk ; 
   reg [10:0] n1 ; 
   reg [7:0] n2 ; 
 
-mult11sx8s u1( .clk(clk), // Invoke the design. 
- .n1(n1), 
- .n2(n2), 
- .result(result) 
- ); 
+mult11sx8s u1( .clk(clk),  .n1(n1),  .n2(n2),  .result(result)  ); 
 initial 
  begin // Apply several sets of inputs. 
  clk = 1'b0 ; 
  
  n1 = 11'h0 ; 
  n2 = 8'h0 ; 
- #17 n1 = 11'h555 ; // Not that the inputs are applied 
+ #7 n1 = 11'h555 ; // Not that the inputs are applied 
  n2 = 8'h55; // before the rising edge of ?clk?. 
- #20 n1 = 11'h2aa ; 
+ #10 n1 = 11'h2aa ; 
  n2 = 8'haa; 
- #20 n1 = 11'h7ff ; 
+ #10 n1 = 11'h7ff ; 
  n2 = 8'h80; 
- #20 n1 = 11'h555 ; 
+ #10 n1 = 11'h555 ; 
  n2 = 8'hff; 
- #20 n1 = 11'h7ff ; 
+ #10 n1 = 11'h7ff ; 
  n2 = 8'h81; 
- #20 n1 = 11'h555 ; 
+ #10 n1 = 11'h555 ; 
  n2 = 8'h81; 
- #20 n1 = 11'h2aa ; 
+ #10 n1 = 11'h2aa ; 
  n2 = 8'h81; 
- #20 n1 = 11'h7ff ; 
+ #10 n1 = 11'h7ff ; 
  n2 = 8'h00; 
- #20 n1 = 11'h7ff ; 
+ #10 n1 = 11'h7ff ; 
  n2 = 8'h7f; 
- #20 n1 = 11'h000 ; 
+ #10 n1 = 11'h000 ; 
  n2 = 8'hff; 
- #20 n1 = 11'h000 ; 
+ #10 n1 = 11'h000 ; 
  n2 = 8'h7f; 
- #400 
+ #200 
+ 
  $stop ; 
 end 
 
--- Adder stuff/mult12sx8u.v
+++ Adder stuff/mult12sx8u.v
@@ -6,7 +6,9 @@ module mult12sx8u( clk, n1, n2, dctq);
   output [8:0] dctq;
   
   wire nlorn2z;
+
   wire dctq_zero;
+
   wire [11:0] p1;
   wire [11:0] p2;
   wire [11:0] p3;
@@ -63,13 +65,13 @@ module mult12sx8u( clk, n1, n2, dctq);
   reg [6:0] s13a_reg2;
   reg [6:0] s14a_reg2;
 
-  reg [11:0] n1_reg1;
-  reg [11:0] n1_reg2;
-  reg [11:0] n1_reg3;
-  reg [11:0] n1_reg4;
-  reg [11:0] n1_reg5;
-  reg [11:0] n1_reg6;
-  reg [11:0] n1_reg7;
+  reg  n1_reg1;
+  reg  n1_reg2;
+  reg  n1_reg3;
+  reg  n1_reg4;
+  reg  n1_reg5;
+  reg  n1_reg6;
+  reg  n1_reg7;
 
   reg n1orn2z_reg1;
   reg n1orn2z_reg2;
@@ -117,14 +119,14 @@ module mult12sx8u( clk, n1, n2, dctq);
   always @(n1)
   begin
     if(n1[11] == 1'b0)
-      n1_mag = n1[11:0];
+      n1_mag <= n1[11:0];
     else
-      n1_mag = ~n1[11:0] + 1;
+      n1_mag <= ~n1[11:0] + 1;
     end
   
   always @(n2)
   begin
-  n2_mag = n2[7:0];
+  n2_mag <= n2[7:0];
   end
  
  assign n1orn2z = ((n1 == 12'b0) || (n2 == 7'b0)) ? 1'b1:1'b0;
@@ -137,12 +139,18 @@ module mult12sx8u( clk, n1, n2, dctq);
  assign p6 = n1_mag[11:0] & {12{n2_mag[5]}};
  assign p7 = n1_mag[11:0] & {12{n2_mag[6]}};
  assign p8 = n1_mag[11:0] & {12{n2_mag[7]}};
- 
- assign s11a = p1_reg1[6:1] + p2_reg1[5:0];//s12a[6:0]
+ //
+ assign s11a = p1_reg1[6:1] + p2_reg1[5:0];//s11a[6:0]
  assign s11b = {1'b0, p1_reg2[11:7]} + p2_reg2[11:6] + s11a_reg2[6];
  
- assign s12a = p3_reg1[6:1] + p4_reg1[5:0];//s13a[6:0]
- assign s12b = {1'b0, p5_reg2[11:7]} + p6_reg2[11:6] + s13a_reg2[6];
+ assign s12a = p3_reg1[6:1] + p4_reg1[5:0];//s12a[6:0]
+ assign s12b = {1'b0, p3_reg2[11:7]} + p4_reg2[11:6] + s12a_reg2[6];
+ 
+ assign s13a = p5_reg1[6:1] + p6_reg1[5:0];//s13a[6:0]
+ assign s13b = {1'b0, p5_reg2[11:7]} + p6_reg2[11:6] + s13a_reg2[6];
+ 
+ assign s14a = p7_reg1[6:1] + p8_reg1[5:0];//s14a[6:0]
+ assign s14b = {1'b0, p7_reg2[11:7]} + p8_reg2[11:6] + s14a_reg2[6];
  
  assign s11 = {s11b, s11a_reg2[5:0], p1_reg2[0]};
  assign s12 = {s12b, s12a_reg2[5:0], p3_reg2[0]};
@@ -183,7 +191,7 @@ module mult12sx8u( clk, n1, n2, dctq);
    
  end
  
- always @ (posedge clk)
+ always @ (posedge clk) //Pipe 2
  begin
    s11a_reg2 <= s11a;
    s12a_reg2 <= s12a;
@@ -191,13 +199,13 @@ module mult12sx8u( clk, n1, n2, dctq);
    s14a_reg2 <= s14a;
    
    p1_reg2[11:7] <= p1_reg1[11:7];
-   p2_reg2[11:7] <= p2_reg1[11:7];
+   p2_reg2[11:6] <= p2_reg1[11:6];
    p3_reg2[11:7] <= p3_reg1[11:7];
-   p4_reg2[11:7] <= p4_reg1[11:7];
+   p4_reg2[11:6] <= p4_reg1[11:6];
    p5_reg2[11:7] <= p5_reg1[11:7];
-   p6_reg2[11:7] <= p6_reg1[11:7];
+   p6_reg2[11:6] <= p6_reg1[11:6];
    p7_reg2[11:7] <= p7_reg1[11:7];
-   p8_reg2[11:7] <= p8_reg1[11:7];
+   p8_reg2[11:6] <= p8_reg1[11:6];
    
    p1_reg2[0] <= p1_reg1[0];
    p3_reg2[0] <= p3_reg1[0];
@@ -208,18 +216,18 @@ module mult12sx8u( clk, n1, n2, dctq);
    n1orn2z_reg2 <= n1orn2z_reg1;
  end
  
- always @ (posedge clk)
+ always @ (posedge clk) //Pipe 3
  begin
-   s11a_reg2 <= s11a;
-   s12a_reg2 <= s12a;
-   s13a_reg2 <= s13a;
-   s14a_reg2 <= s14a;
+   s11_reg3 <= s11;
+   s12_reg3 <= s12;
+   s13_reg3 <= s13;
+   s14_reg3 <= s14;
    
-   n1_reg3 <= n1_reg1;
-   n1orn2z_reg3 <= n1orn2z_reg1;
+   n1_reg3 <= n1_reg2;
+   n1orn2z_reg3 <= n1orn2z_reg2;
  end
  
- always @ (posedge clk)
+ always @ (posedge clk) //Pipe 4
  begin
    s11_reg4[13:9] <= s11_reg3[13:9];
    s11_reg4[1:0]  <= s11_reg3[1:0];
@@ -256,7 +264,7 @@ module mult12sx8u( clk, n1, n2, dctq);
   n1orn2z_reg6 <= n1orn2z_reg5;
   
 end
-
+////////////////////////////////////////////
 always @(posedge clk)
 begin
   s31_reg7 <= s31;
